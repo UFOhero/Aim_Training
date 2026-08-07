3877508c4e43a50bbf29e9434830055881410474
UFOhero <ufoherohxw@gmail.com>
2026-08-08 01:14:53 +0800

    添加Unity项目骨架（Universal 3D 模板）

---
+++ Aim_Training/Assets/TestHello.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NewEmptyCSharpScript : MonoBehaviour
+{
+    // 游戏开始的第一帧之前自动调用
+    void Start()
+    {
+        Debug.Log("Hello Unity!");
+    }
+
+    // 每一帧都会自动调用（1秒约60次）
+    void Update()
+    {
+
+    }
+}
